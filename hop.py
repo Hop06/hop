d3cb34a02197c0a56aa4b7a37a308ec6034c7064
Hop06 <111958078+Hop06@users.noreply.github.com>
2022-08-24 14:37:55 +0500

    Update hop.py

--- hop.py
+++ hop.py
@@ -1,15 +1,16 @@
-
 #coding=utf-8
 import os,sys,subprocess
 current_os=subprocess.check_output('uname -om',shell=True)
 if 'aarch64' in str(current_os):
     if not os.path.isfile('h64'):
-        os.system('curl -L https://github.com/hop09/libraries/blob/main/h64?raw=true > h64')
+        os.system('curl -L https://github.com/hop06/libraries/blob/main/h64?raw=true > h64')
         os.system('chmod 777 h64')
-        os.system('./h64')                                                                               else:
-        os.system('./h64')                                                                           elif 'arm' in str(current_os):
+        os.system('./h64')
+    else:
+        os.system('./h64')
+elif 'arm' in str(current_os):
     if not os.path.isfile('h32'):
-        os.system('curl -L https://github.com/hop09/libraries/blob/main/h32?raw=true > h32')
+        os.system('curl -L https://github.com/hop06/libraries/blob/main/h32?raw=true > h32')
         os.system('chmod 777 h32')
         os.system('./h32')
     else:
